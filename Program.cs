39f00d31ff4c7be68c0c0f62d3e1958a7da96b84
Murilll <murilosertorio25@gmail.com>
2023-07-17 11:18:01 -0300

    Create: create repository

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+ApplicationConfiguration.Initialize();
+
+var form = new Form();
+
+Application.Run(form);
