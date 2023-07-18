311c2214565af2e08e12a593d37756fb62f9160f
Murilll <murilosertorio25@gmail.com>
2023-07-18 08:08:10 -0300

    push

---
+++ human.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+public class Human
+{
+    FlowLayoutPanel panel;
+    Random numberRandom = new Random();
+    int x;
+    int y;
+
+    public Human(Form form)
+    {
+        panel = new FlowLayoutPanel()
+        {
+            BackColor = Color.Green,
+            Location = new Point(numberRandom.Next(0, 1200), numberRandom.Next(0, 1200)),
+            Width = numberRandom.Next(30, 100),
+            Height = numberRandom.Next(30, 100)
+        };
+
+        form.Controls.Add(panel);
+
+        x = panel.Location.X;
+        y = panel.Location.Y;
+    }
+
+    public void go(int PositionPlayerX, int PositionPlayerY, int zombieSpeed)
+    {
+        if (x <= PositionPlayerX && y <= PositionPlayerY)
+        {
+            x += zombieSpeed;
+            y += zombieSpeed;
+        }
+
+        if (x >= PositionPlayerX && y <= PositionPlayerY)
+        {
+            x -= zombieSpeed;
+            y += zombieSpeed;
+        }
+
+        if (x <= PositionPlayerX && y >= PositionPlayerY)
+        {
+            x += zombieSpeed;
+            y -= zombieSpeed;
+        }
+
+        if (x >= PositionPlayerX && y >= PositionPlayerY)
+        {
+            x -= zombieSpeed;
+            y -= zombieSpeed;
+        }
+        
+        panel.Location = new Point(x, y);
+    }
+}
